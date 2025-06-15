198f8d1fd100e0ab6319113bb75f5dcb791de64b
Mr Chamster <s.duda03@gmail.com>
2025-06-15 10:55:14 +0200

    Cube moves definition tamplate

--- src/cube_constructors.cxx
+++ src/cube_constructors.cxx
@@ -4,23 +4,22 @@
 #include <iostream>
 
 
-    Cube::Cube(){
-    }
+Cube::Cube(){
+}
 
-    Cube::Cube(Color wall_front_color,
-         Color wall_right_color,
-         Color wall_left_color,
-         Color wall_up_color,
-         Color wall_down_color,
-         Color wall_back_color) {
-    }
+Cube::Cube(Color wall_front_color,
+        Color wall_right_color,
+        Color wall_left_color,
+        Color wall_up_color,
+        Color wall_down_color,
+        Color wall_back_color) {
+}
 
-    Cube::Cube(Wall wall_front, 
-         Wall wall_right,
-         Wall wall_left,
-         Wall wall_up,
-         Wall wall_down,
-         Wall wall_back) {
-        
-    }
-
+Cube::Cube(Wall wall_front, 
+        Wall wall_right,
+        Wall wall_left,
+        Wall wall_up,
+        Wall wall_down,
+        Wall wall_back) {
+    
+}
