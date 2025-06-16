b2d2a34fda607958bb0dc13bf586dafbc3f3325f
Cadia <nadiafauliii@gmail.com>
2025-06-16 22:17:14 +0200

    omg first move - F

--- src/cube_moves.cxx
+++ src/cube_moves.cxx
@@ -3,24 +3,51 @@
 #include "cube.h"
 #include <iostream>
 
-
+using namespace std;
 
 // ======================================
 // =============== Basic ================
 // ======================================
 void Cube::F(){
+    cout<<"F ";
+    wall_front.rotate_right();
+
+    Color temp1, temp2, temp3;
+    temp1 = wall_up.dl;
+    temp2 = wall_up.dm;
+    temp3 = wall_up.dr;
+
+    wall_up.dl = wall_left.dr;
+    wall_up.dm = wall_left.mr;
+    wall_up.dr = wall_left.tr;
+
+    wall_left.dr = wall_down.tr;
+    wall_left.mr = wall_down.tm;
+    wall_left.tr = wall_down.tl;
+
+    wall_down.tr = wall_right.tl;
+    wall_down.tm = wall_right.ml;
+    wall_down.tl = wall_right.dl;
+
+    wall_right.tl = temp1;
+    wall_right.ml = temp2;
+    wall_right.dl = temp3;
 
 };
 void Cube::R(){
+    cout<<"R ";
 
 };
 void Cube::L(){
+    cout<<"L ";
     
 };
 void Cube::U(){
+    cout<<"U ";
     
 };
 void Cube::D(){
+    cout<<"D ";
     
 };
 
@@ -32,19 +59,19 @@ void Cube::D(){
 // ============== Reverse ==============
 // =====================================
 void Cube::Fp(){
-    
+    cout<<"Fp ";
 };
 void Cube::Rp(){
-    
+    cout<<"Rp ";
 };
 void Cube::Lp(){
-    
+    cout<<"Lp ";
 };
 void Cube::Up(){
-    
+    cout<<"Up ";
 };
 void Cube::Dp(){
-    
+    cout<<"Dp ";
 };
 
 // void Cube::Bp(){};
@@ -57,10 +84,10 @@ void Cube::Dp(){
 // void Cube::x(){};
 
 void Cube::y(){
-    
+    cout<<"y ";
 };
 void Cube::z(){
-    
+    cout<<"z ";
 };
 
 
@@ -71,8 +98,8 @@ void Cube::z(){
 // void Cube::xp(){};
 
 void Cube::yp(){
-    
+    cout<<"yp ";
 };
 void Cube::zp(){
-    
+    cout<<"zp ";
 };
