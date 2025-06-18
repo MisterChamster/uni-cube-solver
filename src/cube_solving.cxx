6e10fd3a00ece5e2ee4cfe3f6d5b9ebcafac06a0
MisterChamster <s.duda03@gmail.com>
2025-06-18 13:23:18 +0200

    white_cross_edge() def update 2

--- src/cube_solving.cxx
+++ src/cube_solving.cxx
@@ -20,13 +20,34 @@ void Cube::white_cross_edge() {
         Color front_color = wall_front.mm;
         //if edge not in the right place
         if(wall_front.dm != front_color || wall_down.tm != Color::white) {
-            //bad orientation
+
+            // =========================================================
+            // ================== White on front wall ==================
+            // =========================================================
             if(wall_front.dm == Color::white && wall_down.tm == front_color) {
                 Fp();
                 R();
                 U();
                 Rp();
-                //F2();
+                F2();
+            }
+            else if(wall_front.mr == Color::white && wall_right.ml == front_color) {
+                R();
+                U();
+                Rp();
+                F2();
+            }
+            else if(wall_front.ml == Color::white && wall_left.mr == front_color) {
+                Lp();
+                Up();
+                L();
+                F2();
+            }
+            else if(wall_front.tm == Color::white && wall_up.dm == front_color) {
+                Up();
+                Rp();
+                F();
+                R();
             }
 
             // =========================================================
@@ -39,12 +60,30 @@ void Cube::white_cross_edge() {
                 Fp();
             }
             else if(wall_front.tm == front_color && wall_up.dm == Color::white) {
-                // F2();
+                F2();
             }
 
-            // =========================================================
-            // ================== White on front wall ==================
-            // =========================================================
+            // ========================================================
+            // =================== White on down wall =================
+            // ========================================================
+            else if(wall_down.mr == Color::white && wall_right.dm == front_color) {
+                R();
+                D();
+                Rp();
+                Dp();
+            }
+            else if(wall_down.ml == Color::white && wall_left.dm == front_color) {
+                L();
+                Dp();
+                Lp();
+                D();
+            }
+            else if(wall_down.dm == Color::white && wall_back.dm == front_color) {
+                D2();
+                F();
+                D2();
+                Fp();
+            }
 
         }
     }
